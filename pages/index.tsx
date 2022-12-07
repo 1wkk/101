db048b980cbfe72878361e32a026329c71e7f4ea
etes-dev <iherewithmyheart@gmail.com>
2022-12-07 17:36:54 +0800

    feat: home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import Rotate101 from '../components/svg/101'
+
 export default function Home() {
-  return <div></div>
+  return (
+    <div className='w-screen h-screen grid place-items-center'>
+      <Rotate101 width={404} height={404} />
+    </div>
+  )
 }
